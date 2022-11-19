4ff23c1e1451d755c6baa27816602e477a24f408
Toubson20 <toubiouf7@gmail.com>
2022-11-20 00:34:40 +0100

    project creation

---
+++ src/test/java/com/PL/PremierLeague/PremierLeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PL.PremierLeague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremierLeagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
